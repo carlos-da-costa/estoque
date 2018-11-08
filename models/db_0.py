84c3af97864758ca1f601086edcc032a42a792e6
Carlos Costa <yamandu.costa@gmail.com>
2018-11-08 15:15:42 -0200

    mais comentários

--- models/db_0.py
+++ models/db_0.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
+
+# variável auxiliar para os tipos de entrada de estoque
+# a chave será gravada no banco (ex. 'e', 's')
+# o valor será exibido no <SELECT>
 TIPO_MOVIMENTO = {'e':'Entrada', 's': 'Saída'}
 
 db.define_table('produto',
                 Field('nome','string',label='Nome do Produto'),
                 Field('estoque_atual','integer',label='Quantidade', default=0),
-                format='%(nome)s'
+                format='%(nome)s' # string exibida nos campos de referência
                 )
 
 
 db.define_table('movimento',
                 Field('tipo',
-                      requires=IS_IN_SET(TIPO_MOVIMENTO),
-                      represent=lambda value, row: TIPO_MOVIMENTO.get(value, '')
+                      requires=IS_IN_SET(TIPO_MOVIMENTO), # valida entrada e monta SELECT no html
+                      represent=lambda value, row: TIPO_MOVIMENTO.get(value, '') # se houver valor no campo mostra o texto correspondente
                       ),
                 Field('produto', db.produto, label='Produto'),
                 Field('quantidade', 'integer', label='Quantidade de Produto'),
